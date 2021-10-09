b2052099c677af2edafc78bcf3cdac734e55000a
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-10-09 07:05:24 +0530

    Add files via upload

---
+++ tkinter_t2_message_box.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+#Add Message Box
+print("Message Box")
+messagebox.showinfo("Python","Python with Tkinter")
+
